22a2ba159b4c9ddf83682890c862f4d1ae72e371
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-17 17:11:31 +0100

    P1: required middleware added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import {config as configDotEnv} from "dotenv";
 
 //Loads the enviroment variables from the .env file
 configDotEnv();
 
-const app = express();
 
 //Starts the webserver at the port provided in the .env file
+const app = express();
 app.listen(process.env.PORT, () => {
     console.log("Lernen is online!")
 })
+
+//Middleware Section
+//This allows cross site requests, for example between the front and backend
+app.use(cors());
+//This means any data sent in a HTTP request body will be automatically parsed into a JSON object
+app.use(bodyParser.json());
+
+
