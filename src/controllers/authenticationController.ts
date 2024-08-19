ba81d37145df524df70d7712e37d022474e4751b
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 15:08:35 +0100

    P1: initial signup test

--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -1,5 +1,11 @@
 import {Request, Response} from 'express';
+import {SignupRequestModel} from "../models/SignupRequestModel";
 
 export const testRequest = (req: Request, res: Response) => {
     res.send('This is a test');
+};
+
+export const signupRequest = (req: Request<{}, {}, SignupRequestModel>, res: Response) => {
+    console.log(req.body);
+    res.send(req.body);
 };
--- src/routes/authenticationRouter.ts
+++ src/routes/authenticationRouter.ts
@@ -1,10 +1,11 @@
 import Router from "express";
-import {testRequest} from "../controllers/authenticationController";
+import {signupRequest, testRequest} from "../controllers/authenticationController";
 
 //Creates a new router
 const authRouter = Router();
 
 authRouter.get("/test", testRequest)
+authRouter.post("/signup", signupRequest)
 
 //Exports the router for use in other files
 export default authRouter;
