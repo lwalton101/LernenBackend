19323a0b381ae5e0d75309f317a092ff7a3d76a9
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 17:51:57 +0100

    P1: add /login to router

--- src/routes/authenticationRouter.ts
+++ src/routes/authenticationRouter.ts
@@ -1,11 +1,12 @@
 import Router from "express";
-import {signupRequest, testRequest} from "../controllers/authenticationController";
+import {loginRequest, signupRequest, testRequest} from "../controllers/authenticationController";
 
 //Creates a new router
 const authRouter = Router();
 
 authRouter.get("/test", testRequest)
 authRouter.post("/signup", signupRequest)
+authRouter.post("/login", loginRequest)
 
 //Exports the router for use in other files
 export default authRouter;
