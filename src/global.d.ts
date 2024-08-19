7c0811f3e10c486c718f530a0e7b307b0bcf0bab
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 15:27:39 +0100

    P1: added db relevant .env vars

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,9 @@
 namespace NodeJS {
     interface ProcessEnv {
         PORT: number;
+        DB_HOST: string;
+        DB_USER: string;
+        DB_PASSWORD: string;
+        DATABASE: string;
     }
 }
