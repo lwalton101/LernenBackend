9ba71001a221de38cf0d7b4e7548036f30ad6309
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 15:18:14 +0100

    P1: username and password verification

--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -6,6 +6,29 @@ export const testRequest = (req: Request, res: Response) => {
 };
 
 export const signupRequest = (req: Request<{}, {}, SignupRequestModel>, res: Response) => {
-    console.log(req.body);
+
+    if (!req.body.username) {
+        res.status(400);
+        res.send({message: "Please include a username"})
+        return;
+    }
+
+    if (!req.body.password) {
+        res.status(400);
+        res.send({message: "Please include a password"})
+        return;
+    }
+    //3 to 16 characters long and contain only letters, digits, underscores, and hyphens
+    if (!req.body.username.match(/^[a-zA-Z0-9_-]{3,16}$/)) {
+        res.status(400);
+        res.send({message: "Invalid Username/Password"})
+        return;
+    }
+    //1 number (0-9),1 uppercase letter,1 lowercase letters,1 non-alpha numeric number,8-16 characters with no space
+    if (!req.body.password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/)) {
+        res.status(400);
+        res.send({message: "Invalid Username/Password"})
+        return;
+    }
     res.send(req.body);
 };
