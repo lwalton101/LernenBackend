b762302539f8daba0494adea35627321bae07460
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 17:51:46 +0100

    P1: fix comparison of passwords and ensure user.userID is not null

--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -84,11 +84,16 @@ export const loginRequest = async (req: Request<{}, {}, LoginRequestModel>, res:
         res.status(400).send({message: "User does not exist"});
         return;
     }
-
-    const compare = await bcrypt.compare(existingUser.password, req.body.password);
+    const hashedPassword = await hashPassword(req.body.password);
+    const compare = await bcrypt.compare(existingUser.password, hashedPassword);
 
     if (!compare) {
         res.status(400).send({message: "Wrong Password"});
+        return;
+    }
+
+    //Should never happen
+    if (!existingUser.userID) {
         return;
     }
 
