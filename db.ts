c978c43b9ec3db1801ddbf256a445e7d5d27e4c5
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 16:05:59 +0100

    P1: database connection actually works, and creates users table

--- db.ts
+++ db.ts
@@ -1,10 +1,42 @@
-import mysql from "mysql2/promise"
+import mysql, {Connection} from "mysql2/promise"
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE
-})
+export let connection: Connection;
 
-export default connection;
+export async function getConnection() {
+    if (!connection) {
+        await initDatabase();
+        return connection;
+    }
+
+    return connection;
+}
+
+export async function initDatabase() {
+    connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    });
+
+    //The query used to create the users table
+    const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS users (
+          userID INT AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL UNIQUE,
+          password VARCHAR(255) NOT NULL,
+          accountCreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          profilePic VARCHAR(255)
+        );
+  `;
+
+    //Execute the query for creating the user table
+    try {
+        await connection.execute(createTableQuery);
+        console.log('Users table created successfully!');
+    } catch (error) {
+        console.error('Error creating table:', error);
+    } finally {
+    }
+}
