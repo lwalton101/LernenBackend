4804ceb2e6e9f77557a945df8aeb14bb384751fc
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-19 23:14:16 +0100

    P1: changed jwt payload to be an object not string

--- src/token.ts
+++ src/token.ts
@@ -1,14 +1,15 @@
 import jwt, {JwtPayload} from "jsonwebtoken";
 
 export function createToken(payload: string): string {
-    return jwt.sign(payload, process.env.JWT_SECRET as string, {expiresIn: 60 * 60});
+    return jwt.sign({payload: payload}, process.env.JWT_SECRET as string, {expiresIn: 60 * 60});
 }
 
 export function verifyToken(token: string): number {
     try {
         // Verify the token using the secret
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-        return parseInt(decoded.data);
+        console.log(decoded)
+        return parseInt(decoded.payload);
     } catch (error) {
         // Handle errors (e.g., token verification failed)
         console.error('Token verification failed:', error);
