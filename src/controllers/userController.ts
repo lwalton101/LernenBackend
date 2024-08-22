24df93a627f41974d40e51b30e9f2f6c3aecdde7
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 14:09:16 +0100

    P2: /user/:id is a protected path as well as BUGFIX: params have to be strings

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,15 @@
 import {Request, Response} from "express";
 import {getUserByID} from "../db/user";
 
-export const getUserByIDRequest = async (req: Request<{ id: number }>, res: Response) => {
-    const user = await getUserByID(req.params.id);
+export const getUserByIDRequest = async (req: Request<{ id: string }>, res: Response) => {
+    const userId = parseInt(req.params.id);
+
+    if (isNaN(userId)) {
+        res.status(400).send({message: "Id must be a number"});
+        return;
+    }
+
+    const user = await getUserByID(userId);
     if (!user) {
         res.status(400).send({message: "No user found with that id"});
         return;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import Router from "express";
 import {getUserByIDRequest} from "../controllers/userController";
+import {authMiddleware} from "../middleware/authMiddleware";
 
 //Creates a new router
 const userRouter = Router();
 
-userRouter.get("/:id", getUserByIDRequest)
+userRouter.get("/:id", authMiddleware, getUserByIDRequest)
 
 //Exports the router for use in other files
 export default userRouter;
