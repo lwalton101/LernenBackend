7aee63561efecc2db88cdcab3c19b3298e9b391a
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 14:05:28 +0100

    P2: return error if user does not exist

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,12 @@
 import {Request, Response} from "express";
+import {getUserByID} from "../db/user";
 
-export const getUserByIDRequest = (req: Request<{ id: number }>, res: Response) => {
-    res.send(req.params.id)
+export const getUserByIDRequest = async (req: Request<{ id: number }>, res: Response) => {
+    const user = await getUserByID(req.params.id);
+    if (!user) {
+        res.status(400).send({message: "No user found with that id"});
+        return;
+    }
+
+    res.send(user);
 };
