f03adb21485344f3d48fe9b79aa85551f0d683a2
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 17:38:43 +0100

    P3: added SQL for all question related tables

---
+++ init.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS users (
+  user_id INT PRIMARY KEY,
+  username VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+  salt VARCHAR(255),
+  account_creation_date TIMESTAMP,
+  profile_pic VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+  question_id INT PRIMARY KEY,
+  title VARCHAR(255),
+  user_id INT,
+  created_at TIMESTAMP,
+  published BOOLEAN,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS subquestions (
+  subquestion_id INT PRIMARY KEY,
+  question_id INT,
+  type INT,
+  question_num INT,
+  text TEXT,
+  audio_file_path VARCHAR(255),
+  option1 VARCHAR(255),
+  option2 VARCHAR(255),
+  option3 VARCHAR(255),
+  option4 VARCHAR(255),
+  answer1 BOOLEAN,
+  answer2 BOOLEAN,
+  answer3 BOOLEAN,
+  answer4 BOOLEAN,
+  FOREIGN KEY (question_id) REFERENCES questions(question_id)
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+  tag_id INT PRIMARY KEY,
+  tag_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS questiontags (
+  question_tag_id INT PRIMARY KEY,
+  question_id INT,
+  tag_id INT,
+  FOREIGN KEY (question_id) REFERENCES questions(question_id),
+  FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+);
