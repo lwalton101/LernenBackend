834b21e68c7f2b56eead5bd1e18e395480f525b0
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 14:15:18 +0100

    P2: added sanitizeUser function

--- src/models/db/SanitizedUser.ts
+++ src/models/db/SanitizedUser.ts
@@ -1,6 +1,17 @@
+import {User} from "./User";
+
 export interface SanitizedUser {
     userID?: number;
     username: string;
     accountCreationDate?: Date;
     profilePic?: string;
+}
+
+export function sanitizeUser(user: User): SanitizedUser {
+    return {
+        userID: user.userID,
+        username: user.username,
+        accountCreationDate: user.accountCreationDate,
+        profilePic: user.profilePic
+    };
 }
