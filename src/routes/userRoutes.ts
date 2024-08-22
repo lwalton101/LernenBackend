cac80333b97935e7d3a6b3d1a72434a009c1442e
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-22 14:00:52 +0100

    P2: BUGFIX: user router was using /user twice

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,7 +4,7 @@ import {getUserByIDRequest} from "../controllers/userController";
 //Creates a new router
 const userRouter = Router();
 
-userRouter.get("/user/:id", getUserByIDRequest)
+userRouter.get("/:id", getUserByIDRequest)
 
 //Exports the router for use in other files
 export default userRouter;
