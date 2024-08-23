49f9090dabfbc9d876e4c19f563b0eae8716ae78
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-23 11:16:20 +0100

    P3: update question endpoint

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -1,9 +1,9 @@
 import {Request, Response} from "express";
 import {CreateQuestionModel, verifyCreateQuestionModel} from "../models/CreateQuestionModel";
-import {createQuestion} from "../db/question";
-import {createSubquestion} from "../db/subquestion";
+import {createQuestion, updateQuestion} from "../db/question";
+import {createSubquestion, deleteSubquestionsByQuestionID} from "../db/subquestion";
 import {createTag} from "../db/tag";
-import {createQuestionTag, getTagsByQuestionID} from "../db/questiontag";
+import {createQuestionTag, deleteQuestionTagsByQuestionID, getTagsByQuestionID} from "../db/questiontag";
 
 export const createQuestionRequest = async (req: Request<{}, {}, CreateQuestionModel>, res: Response) => {
     const error = verifyCreateQuestionModel(req.body);
@@ -47,5 +47,49 @@ export const createQuestionRequest = async (req: Request<{}, {}, CreateQuestionM
     }
 
     res.send({message: "Creating question"})
+};
+
+export const updateQuestionRequest = async (req: Request<{ id: string }, {}, CreateQuestionModel>, res: Response) => {
+    const error = verifyCreateQuestionModel(req.body);
+    if (error) {
+        res.status(400).send({message: error});
+        return;
+    }
+
+    const questionId = parseInt(req.params.id);
+    if (isNaN(questionId)) {
+        res.status(400).send({message: "Question Id must be a number"});
+        return;
+    }
+
+    await updateQuestion(questionId, {
+        created_at: req.body.created_at,
+        published: req.body.published,
+        title: req.body.title,
+        user_id: req.body.user_id
+    });
+
+    await deleteSubquestionsByQuestionID(questionId);
+    for (let subquestion of req.body.subquestions) {
+        subquestion.question_id = questionId;
+        await createSubquestion(subquestion);
+    }
+
+    await deleteQuestionTagsByQuestionID(questionId);
+    let questionTags = (await getTagsByQuestionID(questionId));
+    if (questionTags == null) {
+        res.status(500).send({message: "Unexpected server error"});
+        return;
+    }
+    for (let tag of req.body.tags) {
+        const tagID = await createTag(tag);
+        if (!tagID) {
+            res.status(500).send({message: "Tag ID should not be undefined"});
+            return;
+        }
+        await createQuestionTag(questionId, tagID);
+    }
+
+    res.send({message: "Updated question"})
 };
 
--- src/routes/questionRoutes.ts
+++ src/routes/questionRoutes.ts
@@ -1,11 +1,12 @@
 import Router from "express";
 import {authMiddleware} from "../middleware/authMiddleware";
-import {createQuestionRequest} from "../controllers/questionController";
+import {createQuestionRequest, updateQuestionRequest} from "../controllers/questionController";
 
 //Creates a new router
 const questionRouter = Router();
 
 questionRouter.post("/create", authMiddleware, createQuestionRequest)
+questionRouter.post("/:id/update", authMiddleware, updateQuestionRequest)
 
 //Exports the router for use in other files
 export default questionRouter;
