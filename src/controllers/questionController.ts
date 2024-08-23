42258980bd1bfb262aeb6be71d307884eb91f6f1
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-23 12:24:08 +0100

    P3: fix truthy on a boolean

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -9,6 +9,7 @@ export const createQuestionRequest = async (req: Request<{}, {}, CreateQuestionM
     const error = verifyCreateQuestionModel(req.body);
     if (error) {
         res.status(400).send({message: error});
+        return;
     }
 
     const questionId = await createQuestion({
