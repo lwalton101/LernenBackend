062c9ee0b0ed959d1a186d0d9fae6dded330e604
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-23 10:18:15 +0100

    P3: finish question/create endpoint

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -1,8 +1,12 @@
 import {Request, Response} from "express";
 import {CreateQuestionModel} from "../models/CreateQuestionModel";
 import {verifySubquestion} from "../models/db/Subquestion";
+import {createQuestion} from "../db/question";
+import {createSubquestion} from "../db/subquestion";
+import {createTag} from "../db/tag";
+import {createQuestionTag} from "../db/questiontag";
 
-export const createQuestionRequest = (req: Request<{}, {}, CreateQuestionModel>, res: Response) => {
+export const createQuestionRequest = async (req: Request<{}, {}, CreateQuestionModel>, res: Response) => {
 
     if (!req.body) {
         res.status(400).send({message: "Please include a body!"});
@@ -35,6 +39,42 @@ export const createQuestionRequest = (req: Request<{}, {}, CreateQuestionModel>,
     if (!req.body.published) {
         res.status(400).send({message: "Please include a published status"});
         return;
+    }
+    if (!req.body.tags) {
+        res.status(400).send({message: "Please include at least one tag!"});
+        return;
+    }
+
+    if ((req.body.tags.length > 5)) {
+        res.status(400).send({message: "You can have no more than 5 tags!"});
+        return;
+    }
+
+
+    const questionId = await createQuestion({
+        created_at: req.body.created_at,
+        published: req.body.published,
+        title: req.body.title,
+        user_id: req.body.user_id
+    });
+
+    if (!questionId) {
+        res.status(400).send({message: "Something went wrong!"});
+        return;
+    }
+
+    for (let subquestion of req.body.subquestions) {
+        subquestion.question_id = questionId;
+        await createSubquestion(subquestion);
+    }
+
+    for (let tag of req.body.tags) {
+        const tagID = await createTag(tag);
+        if (!tagID) {
+            res.status(500).send({message: "Tag ID should not be undefined"});
+            return;
+        }
+        await createQuestionTag(questionId, tagID);
     }
 
     res.send({message: "Creating question"})
