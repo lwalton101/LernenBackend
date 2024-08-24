2492e6b30f62c1c8257cea20e8267cb2f50c3ec6
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-08-24 18:20:04 +0100

    P4: add pick random question from db

--- src/db/question.ts
+++ src/db/question.ts
@@ -1,5 +1,5 @@
 import mysql from "mysql2/promise";
-import {Question} from "../models/db/Question";
+import {getFullQuestion, Question} from "../models/db/Question";
 import {pool} from "../db";
 
 export async function createQuestion(question: Question) {
@@ -56,3 +56,41 @@ export async function getQuestion(id: number) {
         return null; // Handle error cases gracefully
     }
 }
+
+export async function pickNRandomQuestions(n: number, userID: string) {
+    if (n < 1) {
+        throw new Error("N must be at least 1");
+    }
+    const query = `
+        SELECT *
+        FROM questions
+        WHERE published = true
+        ORDER BY RAND()
+        LIMIT ?;
+    `;
+
+    const values = [n];
+    try {
+        const [rows] = await pool.execute(query, values);
+        const qS: Question[] = rows as Question[]
+        if (Array.isArray(rows)) {
+            const fullQs = [];
+
+            for (let row of qS) {
+                if (!row.question_id) {
+                    return;
+                }
+                const fullQ = await getFullQuestion(row.question_id, userID)
+                fullQs.push(fullQ);
+            }
+
+            return fullQs;
+        }
+
+        return null; // No question found with the given id
+    } catch (error) {
+        console.error('Error fetching n questions:', error);
+        return null; // Handle error cases gracefully
+    }
+}
+
