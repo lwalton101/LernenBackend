3012ad386285576fb07af3b842a0813fc327d9e2
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-02 16:52:38 +0100

    P6: BUGFIX, spelt something wrong

--- src/routes/searchRouter.ts
+++ src/routes/searchRouter.ts
@@ -1,10 +1,11 @@
 import Router from "express";
 import {authMiddleware} from "../middleware/authMiddleware";
+import {searchRequest} from "../controllers/searchController";
 
 //Creates a new router
 const searchRouter = Router();
 
-searchRouter.post("/", authMiddleware, searchRouter)
+searchRouter.post("/", authMiddleware, searchRequest)
 
 //Exports the router for use in other files
 export default searchRouter;
