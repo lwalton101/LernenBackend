629e8479df0c92f78f6bf9625b753d6ca223b36e
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-03 09:24:09 +0100

    P6: properly censor password

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -100,7 +100,7 @@ export const searchRequest = async (req: Request<{}, {}, SearchQueryModel>, res:
     users = users.sort((a, b) => levenshteinDistance(a.username, req.body.SearchQuery) - levenshteinDistance(b.username, req.body.SearchQuery))
     users = users.slice(0, 10);
     users = users.map((u) => {
-        delete u.password;
+        u.password = "";
         return u;
     })
 
