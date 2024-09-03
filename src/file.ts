b83747065cc6f67e267a74116ff1acbaa3634b40
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-09-03 15:59:21 +0100

    P7: multer middleware

--- src/file.ts
+++ src/file.ts
@@ -1,4 +1,6 @@
 import {S3Client} from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3"
+import multer from "multer";
 
 export const s3client = new S3Client([
     {
@@ -8,3 +10,17 @@ export const s3client = new S3Client([
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
         }
     }]);
+
+
+export const pfpUpload = multer({
+    storage: multerS3({
+        s3: s3client,
+        bucket: 'lernendata',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, `pfp/${file.originalname}`)
+        },
+    })
+})
